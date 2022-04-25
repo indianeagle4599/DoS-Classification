1b1bde5f59ab8539877e5c6f0be05edf3d78ffe2
Ashwin Iyer <ashwiniyer1706@gmail.com>
2022-04-25 21:07:13 +0530

    Update preprocessing

--- notebooks/utils/preprocess.py
+++ notebooks/utils/preprocess.py
@@ -5,18 +5,27 @@ from sklearn.preprocessing import MinMaxScaler
 np.random.seed(1337)
 
 
-def sequential_split(X, Y, test_size=0.25):
-    train_len = int(len(X) * (1 - test_size))
-    X_train = X.iloc[:train_len, :]
-    X_test = X.iloc[train_len:, :]
-    y_train = Y.iloc[:train_len, :]
-    y_test = Y.iloc[train_len:, :]
-    return X_train, X_test, y_train, y_test
+def sequential_split(data, test_size=0.25):
+    """
+    Splits data sequentially based on given dataset and test size
 
-def xy_split(data):
-    X = data.drop(columns=['Y'])
-    Y = pd.get_dummies(data['Y'])
-    return X, Y
+    Args:
+        data (pandas.DataFrame): A Pandas DataFrame containing the entire dataset
+        test_size (float, optional): Proportion of the data to be used as test data and is always a value between 0 and 1. Defaults to 0.25.
+
+    Returns:
+        (train_df,test_df): A tuple containing two Pandas DataFrames, a train and a test set with the same dimensionality as the given DataFrame
+    """
+    train_len = int(len(data) * (1 - test_size))
+    train_df = data.iloc[:train_len, :]
+    test_df = data.iloc[train_len:, :]
+    return train_df, test_df
+
+
+def label_encode(df, col):
+    class_to_idx = {d: i for i, d in enumerate(df[col].unique())}
+    df[col] = df[col].replace(class_to_idx)
+    return df
 
 
 def normalize_data(data, target_col):
@@ -28,9 +37,11 @@ def normalize_data(data, target_col):
 
 
 def preprocess(data):
-    data = pd.get_dummies(data, columns=[1, 2, 3])
+    data = label_encode(data, 1)
+    data = label_encode(data, 2)
+    data = label_encode(data, 3)
     data["Y"] = data.loc[:, 41]
     data.drop(columns=[41], inplace=True)
     data = normalize_data(data, "Y")
-    X, Y = xy_split(data)
-    return sequential_split(X, Y)
+    train_df, test_df = sequential_split(data)
+    return train_df, test_df
